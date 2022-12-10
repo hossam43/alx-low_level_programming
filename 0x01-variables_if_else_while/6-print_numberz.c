234cb10f409dd2c19e38d4b6115908e4fd7006e2
hossam43 <hossamayman876@gmail.com>
2022-12-09 17:57:17 -0800

    add numberz in string

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch;
+for (ch = '0'; ch <= '9'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
