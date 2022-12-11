ba7466db5535676ac22ffc4e407b89b2a7d331c7
hossam43 <hossamayman876@gmail.com>
2022-12-11 10:26:22 -0800

    DRYS

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch, kh;
+for (ch = '0'; ch <= '8'; ch++)
+{
+	for (kh = '1'; kh <= '9'; kh++)
+	{
+	if (ch < kh)
+	{
+	putchar(ch);
+	putchar(kh);
+	if(ch != '8')
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+}
+putchar('\n');
+return (0);
+}
