e415118da98ee05e5b767660b02b49dab3280be4
hossam43 <hossamayman876@gmail.com>
2022-12-11 14:46:43 -0800

    save

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch, kh, oh, sh;
+for (ch = '0'; ch <= '9'; ch++)
+{
+	for (kh = '0'; kh <= '9'; kh++)
+	{
+		for (oh = '0'; oh <= '9'; oh++)
+		{	
+			for (sh = '0'; sh <= '9'; sh++)
+			{
+			if (ch == kh && oh == sh || ch == '0' && kh == '0' || oh > kh && sh > kh && oh >= ch)
+			{	
+			putchar(ch);
+			putchar(kh);
+			putchar(' ');
+			putchar(oh);
+			putchar(sh);
+			putchar (',');
+			putchar (' ');
+			}
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
