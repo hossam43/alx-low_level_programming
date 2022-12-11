f9284c241216dc0fe1ab10f18bd8ad0623bf1b13
hossam43 <hossamayman876@gmail.com>
2022-12-11 07:27:49 -0800

    make the c return 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-write(0,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-return (0);
+{int fd2 = 2;
+write(fd2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
