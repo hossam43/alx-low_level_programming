c3c6dbab569cdf01b4329671db4ee2ba29dc2609
hossam43 <hossamayman876@gmail.com>
2022-12-11 11:16:58 -0800

    add the three digit programme

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch, kh, oh;
+for (ch = '0'; ch <= '8'; ch++)
+{
+	for (kh = '1'; kh <= '9'; kh++)
+	{
+		for (oh = '2'; oh <= '9'; oh ++ )
+		{
+			if (ch < kh && kh < oh )
+			{
+			putchar(ch);
+			putchar(kh);
+			putchar(oh);
+			if (ch != '7')
+			{
+			putchar (',');
+			putchar (' ');
+			}
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
