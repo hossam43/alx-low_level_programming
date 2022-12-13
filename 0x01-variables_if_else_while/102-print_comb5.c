d6f1255cc7a8140523330d881df7a39a781ff752
hossam43 <hossamayman876@gmail.com>
2022-12-12 17:24:02 -0800

    spaces

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,18 +9,18 @@ int main(void)
 int ch, kh;
 for (ch = 0; ch <= 98; ch++)
 {
-	for (kh = ch + 1; kh <= 99; kh++)
-	{			
-			putchar((ch / 10) + '0');
-			putchar((ch % 10) + '0');
-			putchar(' ');
-			putchar((kh / 10) + '0');
-			putchar((kh % 10) + '0');
-			if (ch == 98 && kh == 99)
-				continue;
-			putchar (',');
-			putchar (' ');
-	}
+for (kh = ch + 1; kh <= 99; kh++)
+{
+putchar((ch / 10) + '0');
+putchar((ch % 10) + '0');
+putchar(' ');
+putchar((kh / 10) + '0');
+putchar((kh % 10) + '0');
+if (ch == 98 && kh == 99)
+	continue;
+putchar (',');
+putchar (' ');
+}
 }
 putchar('\n');
 return (0);
