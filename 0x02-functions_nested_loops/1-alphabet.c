5e8c8145c2bca3da338f072529207fc521e7a344
hossam43 <hossamayman876@gmail.com>
2022-12-13 14:06:21 -0800

    add space

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * main - check the code for Holberton School students.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success).
  */
 void print_alphabet(void)
 {
 int ch = 97, kh = 122;
-while(ch <= kh)
+while (ch <= kh)
 {
 _putchar(ch);
 ch++;
