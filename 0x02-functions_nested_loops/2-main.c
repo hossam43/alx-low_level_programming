0beec97c6377c64565740dbfbc0aabfff35dcecc
hossam43 <hossamayman876@gmail.com>
2022-12-13 14:50:36 -0800

    add it 10 times

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int count;
+
+for (count = 0; count <= 9; count++ )
+{ 
+print_alphabet_x10();
+}
+return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - to print alphsbet ten times.
+ * Return: Nothing.
+ */
+void print_alphabet_x10(void)
+{
+int ch = 97, kh = 122;
+while (ch <= kh)
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
+return;
+}
