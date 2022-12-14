6ec1f59470f82591ec52b63c2d9461717b8f9a18
hossam43 <hossamayman876@gmail.com>
2022-12-13 16:41:45 -0800

    check for sign program

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - to check if the entered value is in lowercase or not.
+ * @n: to check the number sign.
+ * Return: 1 for lowercase values.
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if(n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
