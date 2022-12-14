e056025bf5d1594b1d6b3d7d958e7e815ad65308
hossam43 <hossamayman876@gmail.com>
2022-12-13 18:29:37 -0800

    24hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print all miutes and hours in a day
+ * Return: 1 for lowercase values.
+ */
+void jack_bauer(void)
+{
+	int h, m;
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
