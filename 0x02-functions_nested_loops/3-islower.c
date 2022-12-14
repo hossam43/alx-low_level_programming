b445f6cb7483ba41e8a2df4db72a724f7756d86d
hossam43 <hossamayman876@gmail.com>
2022-12-13 16:06:41 -0800

    add styles

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _islower - to check if the entered value is in lowercase or not.
+ *_islower - to check if the entered value is in lowercase or not.
  * Return: 1 for lowercase values.
  */
 int _islower(int c)
 {
 if (c >= 97 && c <= 122)
 {
-return 1;
+return (1);
 }
 else
 {
-return 0;
+return (0);
 }
 }
