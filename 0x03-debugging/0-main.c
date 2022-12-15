0f53910faffcd6a07061afbff0b9996d23493c0d
hossam43 <hossamayman876@gmail.com>
2022-12-15 03:48:51 -0800

    remove some spaces

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
@@ -6,8 +7,8 @@
 
 int main(void)
 {
-        int i;
-        i = 0;
-        positive_or_negative(i);
-        return (0);
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
 }
