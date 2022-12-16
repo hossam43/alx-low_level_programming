9a057d0611990f5e5ae7742759b608bbb0a05a04
hossam43 <hossamayman876@gmail.com>
2022-12-16 14:49:10 -0800

    add tabs

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,22 +7,23 @@
 
 void print_triangle(int size)
 {
-int i, j, k;
+	int i, j, k;
 
-if (size > 0)
-{
-for (i = 1; i <= size; i++)
-{
-for (j = size; j > i; j--)
-{
-_putchar(' ');
-}
-for (k = 1; k <= i; k++)
-{
-_putchar('#');
-}
-_putchar('\n');
-}
-}
-_putchar('\n');
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size; j > i; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = 1; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
 }
