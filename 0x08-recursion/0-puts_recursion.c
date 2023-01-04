8a3a25eb329fef278d6404a38a4e54749ac3bd28
hossam43 <hossamayman876@gmail.com>
2023-01-04 14:07:55 -0800

    add unistd.h

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
  * _puts_recursion - Prints a string, followed by a new line.
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
  * _print_rev_recursion - Prints a string in reverse.
