d54696928639285566c64a58d4c0043a49586eda
hossam43 <hossamayman876@gmail.com>
2023-01-04 23:16:19 -0800

    remove _putchar

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,9 +2,9 @@
 
 #define MAIN_H
 
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
-int _putchar(char c)
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
