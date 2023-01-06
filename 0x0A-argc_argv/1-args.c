cc03d492fffa26d15132a82b244929ae0328e635
hossam43 <hossamayman876@gmail.com>
2023-01-06 12:41:12 -0800

    push

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file followed by new line
+ * @argc: number of argument entered in the command line
+ * @argv: array argument value
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i <= argc; i++)
+	{
+		printf("%s\n",argv[i]);
+	}
+
+	return (0);
+}
