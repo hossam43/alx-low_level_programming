e3dc427c7ba21585679cb9cadc231b5ea77fcf4d
hossam43 <hossamayman876@gmail.com>
2023-01-13 07:54:30 -0800

    delete the printf

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,11 +14,9 @@ char *_strdup(char *str)
 int i;
 char *t;
 int size = sizeof(str);
-printf("The size of the array is: %d\n", size);
 t = malloc((sizeof(str) + 1) * sizeof(char));
 if (str == NULL)
 {
-printf("failed to allocate memory\n");
 return (NULL);
 }
 for (i = 0; i < size; i++)
