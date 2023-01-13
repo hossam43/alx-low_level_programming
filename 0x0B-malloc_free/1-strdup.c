823c99fd5d4cbdcedcb66c83f1432a41ae67ee11
hossam43 <hossamayman876@gmail.com>
2023-01-13 08:57:59 -0800

    add

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,15 +15,11 @@ int i;
 char *t;
 int size = strlen(str);
 
-if (str == NULL)
+if (str == NULL || t == NULL)
 {
 return (NULL);
 }
 t = malloc((size + 1) * sizeof(char));
-if (t == NULL)
-{
-return (NULL);
-}
 for (i = 0; i < size; i++)
 {
 t[i] = str[i];
