18d3e92dbce796ccf439a81ce6fbb89c10231919
hossam43 <hossamayman876@gmail.com>
2023-01-13 07:39:53 -0800

    first commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - create an array using a user define size and char
+ * @str: the string to be copy
+ *
+ * Return: array.
+ */
+
+char *_strdup(char *str)
+{
+int i;
+char *t;
+int size = sizeof(str);
+printf("The size of the array is: %d\n", size);
+t = malloc(sizeof(str) * sizeof(char));
+if (str == NULL)
+{
+printf("failed to allocate memory\n");
+return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+t[i] = str[i];
+}
+return (t);
+}
