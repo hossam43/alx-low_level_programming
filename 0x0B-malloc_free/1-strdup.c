addd574529cd92853e767e335b06539d9a703205
hossam43 <hossamayman876@gmail.com>
2023-01-13 08:32:27 -0800

    string lib

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /**
  * _strdup - create an array using a user define size and char
  * @str: the string to be copy
