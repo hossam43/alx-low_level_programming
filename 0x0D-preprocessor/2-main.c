92729b88567026122ce5a49e39835537d12b912d
hossam43 <hossamayman876@gmail.com>
2023-01-18 13:34:14 -0800

    add

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <string.h>
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int main()
 {
-printf("%s\n", strrchr(__FILE__));
+printf("%s\n",__FILE__);
 return (0);
 }
