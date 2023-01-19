ffc4d35a637a81bbd22e7584743241384783d836
hossam43 <hossamayman876@gmail.com>
2023-01-19 08:32:02 -0800

    accessing the srtcu

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,9 +12,9 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 {
 if (d != NULL)
 {
-d.name = name;
-d.age = age;
-d.owner = owner;
+d-> name;
+d-> age;
+d-> owner;
 }
 return (0);
 }
